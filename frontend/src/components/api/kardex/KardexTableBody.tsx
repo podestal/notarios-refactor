55a257bc93a1da78df2f89f4a096bb4c1978d833
podestal <luisrodriguezpodesta@gmail.com>
2025-05-20 09:52:47 -0400

    table body styled

--- frontend/src/components/api/kardex/KardexTableBody.tsx
+++ frontend/src/components/api/kardex/KardexTableBody.tsx
@@ -8,7 +8,10 @@ const KardexTableBody = ({ kardexList }: Props) => {
   return (
     <div>
         {kardexList.map( singleKardex => (
-            <div key={singleKardex.idkardex}>
+            <div 
+                key={singleKardex.idkardex}
+                className="grid grid-cols-13 text-xs text-center mb-2"
+            >
                 <h2>{singleKardex.kardex}</h2>
                 <p>{singleKardex.fechaingreso}</p>
                 <p>{singleKardex.contrato}</p>
