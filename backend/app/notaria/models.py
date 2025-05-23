6adbe09bfc2affd53d0556b8cf9bdcb99d41da3c
podestal <luisrodriguezpodesta@gmail.com>
2025-05-23 06:01:10 -0400

    contratantes and clientes new models

--- backend/app/notaria/models.py
+++ backend/app/notaria/models.py
@@ -212,3 +212,97 @@ class Tipokar(models.Model):
     class Meta:
         managed = False
         db_table = 'tipokar'
+
+
+class Contratantes(models.Model):
+    """
+    Model representing the contractors in the system.
+    This table stores information about the contractors and their details.
+    """
+
+    idcontratante = models.CharField(primary_key=True, max_length=10)
+    idtipkar = models.IntegerField()
+    kardex = models.CharField(max_length=30, blank=True, null=True)
+    condicion = models.CharField(max_length=100)
+    firma = models.CharField(max_length=3)
+    fechafirma = models.CharField(max_length=10)
+    resfirma = models.IntegerField()
+    tiporepresentacion = models.CharField(max_length=2)
+    idcontratanterp = models.CharField(max_length=3000)
+    idsedereg = models.CharField(max_length=3, blank=True, null=True)
+    numpartida = models.CharField(max_length=50, blank=True, null=True)
+    facultades = models.CharField(max_length=500)
+    indice = models.CharField(max_length=3)
+    visita = models.CharField(max_length=3)
+    inscrito = models.CharField(max_length=1, blank=True, null=True)
+    plantilla = models.CharField(max_length=3, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'contratantes'
+
+
+class Cliente2(models.Model):
+    """
+    Model representing the client in the system.
+    This table stores information about the clients and their details.
+    """
+
+    idcontratante = models.CharField(primary_key=True, max_length=10)
+    idcliente = models.CharField(max_length=10)
+    tipper = models.CharField(max_length=1)
+    apepat = models.CharField(max_length=100)
+    apemat = models.CharField(max_length=100)
+    prinom = models.CharField(max_length=100)
+    segnom = models.CharField(max_length=100)
+    nombre = models.CharField(max_length=1000)
+    direccion = models.CharField(max_length=3000)
+    idtipdoc = models.IntegerField()
+    numdoc = models.CharField(max_length=50)
+    email = models.CharField(max_length=300)
+    telfijo = models.CharField(max_length=20)
+    telcel = models.CharField(max_length=20)
+    telofi = models.CharField(max_length=20)
+    sexo = models.CharField(max_length=1)
+    idestcivil = models.IntegerField()
+    natper = models.CharField(max_length=50)
+    conyuge = models.CharField(max_length=10)
+    nacionalidad = models.CharField(max_length=100)
+    idprofesion = models.IntegerField()
+    detaprofesion = models.CharField(max_length=1000, blank=True, null=True)
+    idcargoprofe = models.IntegerField()
+    profocupa = models.CharField(max_length=1000)
+    dirfer = models.CharField(max_length=300)
+    idubigeo = models.CharField(max_length=6)
+    cumpclie = models.CharField(max_length=15)
+    fechaing = models.CharField(max_length=10)
+    razonsocial = models.CharField(max_length=3000)
+    domfiscal = models.CharField(max_length=3000)
+    telempresa = models.CharField(max_length=12)
+    mailempresa = models.CharField(max_length=200)
+    contacempresa = models.CharField(max_length=3000)
+    fechaconstitu = models.CharField(max_length=12)
+    idsedereg = models.IntegerField()
+    numregistro = models.CharField(max_length=50)
+    numpartida = models.CharField(max_length=50)
+    actmunicipal = models.CharField(max_length=3000)
+    tipocli = models.CharField(max_length=1)
+    impeingre = models.CharField(max_length=10)
+    impnumof = models.CharField(max_length=50)
+    impeorigen = models.CharField(max_length=3000)
+    impentidad = models.CharField(max_length=3000)
+    impremite = models.CharField(max_length=3000)
+    impmotivo = models.CharField(max_length=3000)
+    residente = models.CharField(max_length=2)
+    docpaisemi = models.CharField(max_length=100)
+    partidaconyuge = models.CharField(max_length=15, blank=True, null=True)
+    separaciondebienes = models.CharField(max_length=1, blank=True, null=True)
+    idsedeconyuge = models.CharField(max_length=11, blank=True, null=True)
+    profesion_plantilla = models.CharField(
+        max_length=200, blank=True, null=True
+    )
+    ubigeo_plantilla = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'cliente2'
